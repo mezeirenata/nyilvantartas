a0bc27195b066027abfe273a8a0b6b775f66c285
Renata Mezei <mezei.renata@students.jedlik.eu>
2024-03-12 12:25:42 +0100

    #6 osztályok, nincs kész, haladás + jegyek nevek, még meg kell csinálnom az egyéb adatokat és összeköttetéseket

--- jegyek.py
+++ jegyek.py
@@ -1,48 +1,46 @@
-from diak import Diak
 
 class Jegyek:
     def __init__(self, sor) -> None:
         data = sor.strip().split(";")
-        self.nev,self.osztaly,self.email,self.jelszo,self.osztondij = self.nevDiak('diakok.csv', data[0])
-        #elsődleges kulcs
-        self.jegyek = self.jegyekDict()
-
-    def jegyekDict(self):
-        dictionary = {}
-        f = open("csv/jegyek.csv", "r", encoding="utf-8")
-        for i, sor in enumerate(f):
-            splitted = sor.strip().split(";")
-            if i == 0:
-                targyak = sor.strip().split(";")
-                for t in targyak:
-                    if t != "nev":
-                        dictionary[t] = []
-            else:
-                if splitted[0] == self.nev:
-                    for x, s in enumerate(splitted[1:]):
-                        splitted2 = s.split(",")
-                        for a in splitted2:
-                            keys = list(dictionary)
-                            dictionary[keys[x]].append(int(a))
-
-        f.close()
-        return dictionary
+        self.nev = data[0]
+        #elsődleges kulcs ^
+        self.jegyek = str(data[1:])
+        self.tortenelem,self.informatika,self.angol,self.irodalom,self.nyelvtan,self.testneveles,self.matek,self.fizika = self.jegyek.strip(';')
         
-    def nevDiak(self, filename:str, nev:str) -> tuple:
-        f = open(f"csv/{filename}", 'r', encoding= 'utf-8')
-        elsodleges_kulcs = 'None'
-        f.readline()
-        for sor in f:
-            splitted = sor.split(';')
-            if nev == str(splitted[0]):
-                self.nev = str(splitted[0])
-                self.osztaly = splitted[1]
-                self.email = splitted[2]
-                self.jelszo = splitted[3]
-                self.osztondij = int(splitted[4])
-                return self.nev,self.osztaly,self.email,self.jelszo,self.osztondij
+        self.tortenelem = self.tantargy("tortenelem")
+        self.informatika = self.tantargy("informatika")
+        self.angol = self.tantargy("angol")
+        self.irodalom = self.tantargy("irodalom")
+        self.nyelvtan = self.tantargy("nyelvtan")
+        self.testneveles = self.tantargy("testneveles")
+        self.matek = self.tantargy("matek")
+        self.fizika = self.tantargy("fizika")
+
+        self.
+
+
+    def tantargy(self, tantargy:str) -> list:
+        list = []
+        for jegy in self.tantargy:
+            list.append(int(jegy))
+        return list
+
+
+    # def nevDiak(self, filename:str, nev:str) -> tuple:
+    #     f = open(f"csv/{filename}", 'r', encoding= 'utf-8')
+    #     elsodleges_kulcs = 'None'
+    #     f.readline()
+    #     for sor in f:
+    #         splitted = sor.split(';')
+    #         if nev == str(splitted[0]):
+    #             self.nev = str(splitted[0])
+    #             self.osztaly = splitted[1]
+    #             self.email = splitted[2]
+    #             self.jelszo = splitted[3]
+    #             self.osztondij = int(splitted[4])
+    #             return self.nev,self.osztaly,self.email,self.jelszo,self.osztondij
             
-        f.close()
+    #     f.close()
 
 
 # nev;tortenelem;informatika;angol;irodalom;nyelvtan;testneveles;matek;fizika
